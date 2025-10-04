36eef22fce2f6417b09a7c49506ea370c751f830
Kacper Winiarczyk <kwiniarczyk99@gmail.com>
2025-10-04 19:04:32 +0200

    .

--- dane_publiczne.py
+++ dane_publiczne.py
@@ -1,19 +1,25 @@
-import os
-from csv_download import download_file
 from datetime import datetime
+from pathlib import Path
 
+from csv_download import download_file
 
 if __name__ == '__main__':
     #INFO: fetch csv from drive
-    base_path_public_path = '/home/kacper/projects/dane-publiczne'
-    today = datetime.today().strftime('%Y-%m-%d')
-    file_name = f'ceny-ofertowe-mieszkan-dewelopera-arcus-{today}.csv'
-    file_path = os.path.join(base_path_public_path, file_name)
+    today = datetime.today()
+
+    path = f'/home/dane-publiczne/data/dane-publiczne/{today.strftime("%Y/%m")}'
+    path = Path(path)
+    path.mkdir(parents=True, exist_ok=True)
+
+    file_name = f'arcus-{today.day}.csv'
+    file_path = path / file_name
+
     download_file(
-        sa_json_path='/home/kacper/projects/dane-publiczne/pod-manage-db493003b6e1.json',
+        sa_json_path='/home/dane-publiczne/script/pod-manage-db493003b6e1.json',
         file_id='1PtFy7D19QbFlH5sBajwTNrDj6MCLFjct3EaijxzctNo',
-        out_path=file_path,
+        out_path=file_path
     )
+
 
     #INFO: Load existing XML
     xml_file_path = '/home/kacper/projects/dane-publiczne/Szablon_budowy_pliku_xml_v.1.13_21.08.2025.xml'
