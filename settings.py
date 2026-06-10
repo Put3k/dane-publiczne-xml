6d37f6957cc83a43e39bda3fe8e561d9142ad3b2
Kacper Winiarczyk <kwiniarczyk99@gmail.com>
2026-06-10 22:50:44 +0200

    added: dotenv

--- settings.py
+++ settings.py
@@ -2,7 +2,11 @@
 from pathlib import Path
 import os
 
+from dotenv import load_dotenv
+
 BASE_DIR = Path(os.path.dirname(os.path.abspath(__file__)))
+
+load_dotenv(BASE_DIR / '.env')
 
 BASE_PUBLIC_DATA_PATH = BASE_DIR / 'test'
 DEVELOPERS_JSON_PATH = BASE_DIR / 'developers_data.json'
